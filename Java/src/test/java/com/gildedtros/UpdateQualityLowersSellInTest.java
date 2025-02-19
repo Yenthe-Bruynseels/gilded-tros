fbd5c1f9241c1c1e1b13de7772a6c22c37709d5c
yenthebruynseels <yenthe00@gmail.com>
2025-02-19 23:19:04 +0100

    Add tests to check make sure updateQuality() updates sellIn correctly

---
+++ Java/src/test/java/com/gildedtros/UpdateQualityLowersSellInTest.java
@@ -0,0 +1,34 @@
+package com.gildedtros;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UpdateQualityLowersSellInTest {
+
+    @Test
+    void updateQualityLowersSellIn() {
+        Item[] items = new Item[] {
+                new Item("eenItem", 2, 5)
+        };
+
+        GildedTros gildedTros = new GildedTros(items);
+
+        gildedTros.updateQuality();
+
+        assertEquals(1, gildedTros.items[0].sellIn);
+    }
+
+    @Test
+    void updateQualityDoesNotLowerSellInForLegendaryItems() {
+        Item[] items = new Item[] {
+                new Item("B-DAWG Keychain", 2, 5)
+        };
+
+        GildedTros gildedTros = new GildedTros(items);
+
+        gildedTros.updateQuality();
+
+        assertEquals(2, gildedTros.items[0].sellIn);
+    }
+}
