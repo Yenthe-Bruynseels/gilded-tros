3a27dbccd580b005b78c7ba45da5ad34d23fd47b
yenthebruynseels <yenthe00@gmail.com>
2025-02-20 17:06:50 +0100

    Extended existing tests and added tests for Smelly Items
    Note: This build will fail, as it meant for TDD

--- Java/src/test/java/com/gildedtros/UpdateQualityUpdatesQualityTest.java
+++ Java/src/test/java/com/gildedtros/UpdateQualityUpdatesQualityTest.java
@@ -3,6 +3,7 @@ package com.gildedtros;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -10,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UpdateQualityUpdatesQualityTest {
 
-    @Test
-    void updateQualityLowersQualityByOneForNormalItemsIfSellInisMoreThanZero() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"eenItem"})
+    void updateQualityLowersQualityByOneForNormalItemsIfSellInisMoreThanZero(String name) {
         Item[] items = new Item[] {
-                new Item("eenItem", 1, 5)
+                new Item(name, 1, 5)
         };
 
         GildedTros gildedTros = new GildedTros(items);
@@ -39,7 +42,7 @@ public class UpdateQualityUpdatesQualityTest {
 
     @ParameterizedTest
     @CsvSource({"1,0","0,0","0,1","-1,0","-1,0"})
-    void updateQualityDoesNotLowerQualityBelowZero(int sellIn, int quality) {
+    void updateQualityDoesNotLowerQualityBelowZeroForNormalItems(int sellIn, int quality) {
         Item[] items = new Item[] {
                 new Item("eenItem", sellIn, quality)
         };
@@ -183,14 +186,64 @@ public class UpdateQualityUpdatesQualityTest {
         assertEquals(50, gildedTros.items[0].quality);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Duplicate Code", "Long Methods", "Ugly Variable Names"})
+    void updateQualityLowerQualityByTwoForSmellyItemsWhenSellInDateHasNotPassed(String name) {
+        Item[] items = new Item[] {
+                new Item(name, 1, 5)
+        };
+
+        GildedTros gildedTros = new GildedTros(items);
+
+        gildedTros.updateQuality();
+
+        assertEquals(3, gildedTros.items[0].quality);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Duplicate Code", "Long Methods", "Ugly Variable Names"})
+    void updateQualityLowerQualityByFourForSmellyItemsWhenSellInDateHasPassed(String name) {
+        Item[] items = new Item[] {
+                new Item(name, 0, 5)
+        };
+
+        GildedTros gildedTros = new GildedTros(items);
+
+        gildedTros.updateQuality();
+
+        assertEquals(1, gildedTros.items[0].quality);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,0", "1,1", "0,0", "0,1", "0,2", "0,3", "-1,0", "-1,1", "-1,2", "-1,3"})
+    void updateQualityDoesNotLowerQualityBelowZeroForSmellyItems(int sellIn, int quality) {
+        Item[] items = new Item[] {
+                new Item("Duplicate Code", sellIn, quality),
+                new Item("Long Methods", sellIn, quality),
+                new Item("Ugly Variable Names", sellIn, quality)
+        };
+
+        GildedTros gildedTros = new GildedTros(items);
+
+        gildedTros.updateQuality();
+
+        assertEquals(0, gildedTros.items[0].quality);
+        assertEquals(0, gildedTros.items[1].quality);
+        assertEquals(0, gildedTros.items[2].quality);
+    }
+
     @Test
     void combinationOfItemsUpdatesAllQualitiesCorrectly() {
         Item[] items = new Item[] {
                 new Item("Backstage passes for Re:Factor", 11, 5),
-                new Item("Backstage passes for HAXX", 10, 5),
                 new Item("B-DAWG Keychain", 11, 5),
+                new Item("Long Methods", 11, 5),
                 new Item("Good Wine", 11, 5),
-                new Item("eenItem", 11, 5)
+                new Item("eenItem", 11, 5),
+                new Item(null, 0, 5),
+                new Item("Duplicate Code", 0, 5),
+                new Item("Backstage passes for HAXX", 10, 5),
+                new Item("Ugly Variable Names", 11, 5)
         };
 
         GildedTros gildedTros = new GildedTros(items);
@@ -198,9 +251,13 @@ public class UpdateQualityUpdatesQualityTest {
         gildedTros.updateQuality();
 
         assertAll(() -> assertEquals(6, gildedTros.items[0].quality),
-                () -> assertEquals(7, gildedTros.items[1].quality),
-                () -> assertEquals(5, gildedTros.items[2].quality),
+                () -> assertEquals(5, gildedTros.items[1].quality),
+                () -> assertEquals(3, gildedTros.items[2].quality),
                 () -> assertEquals(6, gildedTros.items[3].quality),
-                () -> assertEquals(4, gildedTros.items[4].quality));
+                () -> assertEquals(4, gildedTros.items[4].quality),
+                () -> assertEquals(3, gildedTros.items[5].quality),
+                () -> assertEquals(1, gildedTros.items[6].quality),
+                () -> assertEquals(7, gildedTros.items[7].quality),
+                () -> assertEquals(3, gildedTros.items[8].quality));
     }
 }
--- Java/src/test/java/com/gildedtros/UpdateQualityUpdatesSellInTest.java
+++ Java/src/test/java/com/gildedtros/UpdateQualityUpdatesSellInTest.java
@@ -1,16 +1,28 @@
 package com.gildedtros;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UpdateQualityUpdatesSellInTest {
 
-    @Test
-    void updateQualityLowersSellIn() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+            "Backstage passes for Re:Factor",
+            "Long Methods",
+            "Good Wine",
+            "eenItem",
+            "Duplicate Code",
+            "Backstage passes for HAXX",
+            "Ugly Variable Names"})
+    void updateQualityLowersSellIn(String name) {
         Item[] items = new Item[] {
-                new Item("eenItem", 2, 5)
+                new Item(name, 2, 5)
         };
 
         GildedTros gildedTros = new GildedTros(items);
@@ -37,10 +49,14 @@ public class UpdateQualityUpdatesSellInTest {
     void combinationOfItemsUpdatesAllQualitiesCorrectly() {
         Item[] items = new Item[] {
                 new Item("Backstage passes for Re:Factor", 11, 5),
-                new Item("Backstage passes for HAXX", 10, 5),
                 new Item("B-DAWG Keychain", 11, 5),
+                new Item("Long Methods", 11, 5),
                 new Item("Good Wine", 11, 5),
-                new Item("eenItem", 11, 5)
+                new Item("eenItem", 11, 5),
+                new Item(null, 11, 5),
+                new Item("Duplicate Code", 11, 5),
+                new Item("Backstage passes for HAXX", 10, 5),
+                new Item("Ugly Variable Names", 11, 5)
         };
 
         GildedTros gildedTros = new GildedTros(items);
@@ -48,9 +64,13 @@ public class UpdateQualityUpdatesSellInTest {
         gildedTros.updateQuality();
 
         assertAll(() -> assertEquals(10, gildedTros.items[0].sellIn),
-                () -> assertEquals(9, gildedTros.items[1].sellIn),
-                () -> assertEquals(11, gildedTros.items[2].sellIn),
+                () -> assertEquals(11, gildedTros.items[1].sellIn),
+                () -> assertEquals(10, gildedTros.items[2].sellIn),
                 () -> assertEquals(10, gildedTros.items[3].sellIn),
-                () -> assertEquals(10, gildedTros.items[4].sellIn));
+                () -> assertEquals(10, gildedTros.items[4].sellIn),
+                () -> assertEquals(10, gildedTros.items[5].sellIn),
+                () -> assertEquals(10, gildedTros.items[6].sellIn),
+                () -> assertEquals(9, gildedTros.items[7].sellIn),
+                () -> assertEquals(10, gildedTros.items[8].sellIn));
     }
 }
